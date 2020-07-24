4fb521a3106762a9561396a7e6ae8962ca18e36d
zappin <61209204+zappin-ctrl@users.noreply.github.com>
2020-07-24 12:55:22 +0300

    Fixing bugs
    Emotes with numbers in the name wouldnt work, also its now an embed with some extra info

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,26 +1,37 @@
 exports.run = async (client, message, args) => {
  const emoji = args[0]
   if(!emoji) return message.reply('Please define emoji to search for.');
-
-  const emojiID = emoji.replace(/[^0-9]+/g, '');
   let filer
+  let embed
   if (emoji.includes(`<a:`, 0) == true) {
-     filer = `https://cdn.discordapp.com/emojis/${emojiID}.gif`
+     const test = emoji.replace(/\<a:(.*?)\:+/g, '').replace(/>+/g, '')
+     filer = `https://cdn.discordapp.com/emojis/${test}.gif`
+     	 embed = {
+        "description": `Type: **Animated(.gif)** \n ID: **${test}**`,
+        "color": process.env.COLOUR,
+        "image": {
+           "url": filer
+        },
+    };
   } else if (emoji.includes(`<:`, 0) == true) { 
-     filer = `https://cdn.discordapp.com/emojis/${emojiID}.png`
+     const test = emoji.replace(/\<:(.*?)\:+/g, '').replace(/>+/g, '')
+     filer = `https://cdn.discordapp.com/emojis/${test}.png`
+    	 embed = {
+       "description": `Type: **Image(.png)** \n ID: **${test}**`,
+       "color": process.env.COLOUR,
+       "image": {
+         "url": filer
+        },
+    };
   } else {
-    let filer = `Not an existing emoji.`
+        	 embed = {
+       "description": `**Seems to be an invalid emoji. Please try a different one.**`,
+       "color": process.env.COLOUR
+    };
   }
-    if (filer == `Not an existing emoji.`) {
-    return message.channel.send(filer)
-  } else {
- return message.channel.send({
-  files: [{
-    attachment: filer,
-  }]
- }) 
+  return message.channel.send({ embed });
   }
-}
+
 
     exports.conf = {
     aliases: ["emote","enlarge","steal"]
