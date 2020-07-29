ca129edcf590b8a2571eb0bbd0b416f36c02d15b
zappin <61209204+zappin-ctrl@users.noreply.github.com>
2020-07-30 02:26:00 +0300

    Update choose.js

--- commands/choose.js
+++ commands/choose.js
@@ -1,8 +1,8 @@
-exports.run = (client, message, args) => {
+exports.run = (client, message, args, argsclean) => {
   if (!args[0]) {
     message.channel.send(`Please type your options separated with a comma.\n**Example: \`+choose option 1,option 2,etc\`**`)
   } else {
-let arg = args.join(" ")
+let arg = argsclean.join(" ")
 let ar = arg.split(`,`)
 let random = Math.floor(Math.random() * ar.length);
 let answer = ar[random]
