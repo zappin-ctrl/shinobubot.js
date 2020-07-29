2d68d2f48241e25c26b7f715fb1f4580d3a393b7
zappin <61209204+zappin-ctrl@users.noreply.github.com>
2020-07-30 02:01:55 +0300

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,13 @@
+exports.run = async (client, message, args) => { 
+if (!args[0]) {
+  return message.channel.send(`Please type something for me to repeat.`)
+} else {
+let say = message.cleanContent.replace(`+say`,"").replace(`+echo`,"")
+message.delete({ timeout: 10000 }).catch(O_o=>{}); 
+return message.channel.send(say)
+}
+}
+
+    exports.conf = {
+    aliases: ["echo"]
+  }
