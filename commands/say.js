85b07e1252a267895ac9a5eaa01e951cc803a577
zappin <61209204+zappin-ctrl@users.noreply.github.com>
2020-07-30 02:28:10 +0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,8 +1,8 @@
-exports.run = async (client, message, args) => { 
+exports.run = async (client, message, args, argsclean) => { 
 if (!args[0]) {
   return message.channel.send(`Please type something for me to repeat.`)
 } else {
-let say = message.cleanContent.replace(`+say`,"").replace(`+echo`,"")
+let say = argsclean.join(" ")
 message.delete({ timeout: 10000 }).catch(O_o=>{}); 
 return message.channel.send(say)
 }
