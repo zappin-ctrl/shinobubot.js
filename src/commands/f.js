8ce3b7c7eaf61321fcfdf72ca12cddbeb3b1cda9
zappin <61209204+zappin-ctrl@users.noreply.github.com>
2020-08-08 23:37:43 +0300

    Removed message author mention

--- src/commands/f.js
+++ src/commands/f.js
@@ -3,9 +3,9 @@ import {removeCommandPart} from "../utility";
 export const aliases = ["rip"];
 export const run = async (message, args) => {
     if (args[0]) {
-        await message.channel.send(`**${process.env.DEAD_EMOTE} <@${message.author.id}>** paid their respects for **${removeCommandPart(message.cleanContent)}**.`)
+        await message.channel.send(`**${process.env.DEAD_EMOTE} ${message.author.username}** paid their respects for **${removeCommandPart(message.cleanContent)}**.`)
     } else {
-        await message.channel.send(`**${process.env.DEAD_EMOTE} <@${message.author.id}>** paid their respects.`)
+        await message.channel.send(`**${process.env.DEAD_EMOTE} ${message.author.username}** paid their respects.`)
     }
 };
 
