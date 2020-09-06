6414fa04ca0c6ee9e47391d196cbfa7bc89db2af
Ly <kakolp95@gmail.com>
2020-09-06 17:26:28 +0200

    Code cleanup for weather file and version bump

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -4,73 +4,65 @@ import Canvas from "canvas";
 import { registerFont } from 'canvas';
 
 export const run = async (message, args) => {
-  try {
-    message.channel.startTyping();
+    await message.channel.startTyping();
     let question = encodeURI(args.join('+'));
-
     if (!question) {
         return message.channel.send("Please type out a valid location.");
     }
-    
-    let weather;
 
-    weather = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${question}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`);
-    
-    if (weather.data.cod === 200) {
-      let mode = `#000000`
-      let light = `#ffe959`;
-      let flag = `https://www.countryflags.io/${weather.data.sys.country}/flat/64.png`
-      let icon = `http://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`;
-      let filename;
-      //var date = parseInt(new Date(new Date().getTime()).toString().split(" ").slice(4,5).toString().substr(0,2));
-      //var test = ((new Date().getTime())+(3600000 * (weather.data.timezone/3600)))
-      if (weather.data.dt > weather.data.sys.sunset || weather.data.dt<weather.data.sys.sunrise) {
-        light = `#43445c`;
-        mode = `#FFFFFF`;
-      }
-      registerFont('./assets/fonts/Roboto.ttf', { family: 'Roboto' });
-      const canvas = Canvas.createCanvas(200, 200);
-  	  const ctx = canvas.getContext('2d');
-      var x = canvas.width / 2;
-      var y = canvas.height / 2;
-      ctx.beginPath();
-      ctx.rect(0, 0, 200, 200);
-      ctx.fillStyle = `${light}`;
-      ctx.fill();
+    try {
+        const weather = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${question}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`);
 
-      ctx.globalAlpha = 0.2;
-      ctx.beginPath();
-      ctx.rect(0, 10, 200, 84);
-      ctx.fillStyle = `${mode}`;
-      ctx.fill();
+        if (weather.data.cod !== 200) {
+            await message.channel.send(`Please use a valid location's name.`);
+            await message.channel.stopTyping();
+            return;
+        }
 
-      ctx.globalAlpha = 1;
+        let mode = `#000000`
+        let light = `#ffe959`;
+        const flag = `https://www.countryflags.io/${weather.data.sys.country}/flat/64.png`
+        const icon = `http://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`;
+        if (weather.data.dt > weather.data.sys.sunset || weather.data.dt<weather.data.sys.sunrise) {
+            light = `#43445c`;
+            mode = `#FFFFFF`;
+        }
+        registerFont('./assets/fonts/Roboto.ttf', { family: 'Roboto' });
+        const canvas = Canvas.createCanvas(200, 200);
+        const ctx = canvas.getContext('2d');
+        const x = canvas.width / 2;
+        ctx.beginPath();
+        ctx.rect(0, 0, 200, 200);
+        ctx.fillStyle = `${light}`;
+        ctx.fill();
 
-      ctx.font = "48px Roboto";
-      ctx.textAlign = 'center';
-      ctx.fillStyle = `${mode}`;
-      ctx.fillText(`${parseInt(weather.data.main.temp)}°C`, x, 150); 
-      ctx.font = "23px Roboto";
-      ctx.fillText(`${weather.data.weather[0].main.toLowerCase()}`, x, 180);
-      ctx.font = "14px Roboto";
-      ctx.fillText(`${weather.data.name}`, x, 82);
+        ctx.globalAlpha = 0.2;
+        ctx.beginPath();
+        ctx.rect(0, 10, 200, 84);
+        ctx.fillStyle = `${mode}`;
+        ctx.fill();
 
-      const flagimage = await Canvas.loadImage(flag);
-	    ctx.drawImage(flagimage, -1, 174, 32, 32);
+        ctx.globalAlpha = 1;
 
-      const image = await Canvas.loadImage(icon);
-	    ctx.drawImage(image, 60, -1, 80, 80);
+        ctx.font = "48px Roboto";
+        ctx.textAlign = 'center';
+        ctx.fillStyle = `${mode}`;
+        ctx.fillText(`${parseInt(weather.data.main.temp)}°C`, x, 150);
+        ctx.font = "23px Roboto";
+        ctx.fillText(`${weather.data.weather[0].main.toLowerCase()}`, x, 180);
+        ctx.font = "14px Roboto";
+        ctx.fillText(`${weather.data.name}`, x, 82);
 
-      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'weather.jpg');
-      message.channel.send(attachment);
-    } else {
-      message.channel.send(`Please use a valid location's name.`);
+        ctx.drawImage(await Canvas.loadImage(flag), -1, 174, 32, 32);
+
+        ctx.drawImage(await Canvas.loadImage(icon), 60, -1, 80, 80);
+
+        await message.channel.send(new Discord.MessageAttachment(canvas.toBuffer(), 'weather.jpg'));
+    } catch {
+        await message.channel.send(`Something went wrong, try another location.`);
+    } finally {
+        await message.channel.stopTyping();
     }
-  } catch {
-    message.channel.send(`Something went wrong, try another location.`)
-  } finally {
-    message.channel.stopTyping();
-  }
 };
 
 export const help = 'See the weather conditions in the location of your choice';
