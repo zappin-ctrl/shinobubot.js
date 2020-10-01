4c6e0b36590b7cf3adbbcd2a37cc577525a8efe5
zappin <61209204+zappin-ctrl@users.noreply.github.com>
2020-10-01 13:37:45 +0300

    Remove magik

--- src/canvasTransformLogic.js
+++ src/canvasTransformLogic.js
@@ -1,11 +1,1 @@
-import Canvas from "canvas";
-import axios from "axios";
 
-export const magik = async (ctx, image, canvas, imageSrc) => {
-    let response = await axios.get(`https://api.alexflipnote.dev/filter/magik?image=${imageSrc}`, {
-        responseType: "arraybuffer"
-    });
-    const base64 = Buffer.from(response.data, 'binary').toString('base64');
-
-    ctx.drawImage(await Canvas.loadImage('data:' + response.headers['content-type'] + ';base64,' + base64), 0, 0, canvas.width, canvas.height);
-};
