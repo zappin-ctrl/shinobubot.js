73d729cae735c2000ba29ab9c60257fd87d6a0f5
zappin-ctrl <titos98@windowslive.com>
2020-10-08 20:08:16 +0300

    Fast spawns testing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "dotenv/config"
 import logger from "./logger";
 import client from "./bot";
 import axios from "axios";
-import {getUserAgent} from "./utility";
+import { getUserAgent } from "./utility";
 import sequelize from "./sequelize";
 import spoopy from "./auto/spoopy";
 import { registerFont } from 'canvas';
@@ -15,13 +15,13 @@ registerFont('./assets/fonts/Roboto.ttf', { family: 'Roboto' });
 
 axios.defaults.headers.common['User-Agent'] = getUserAgent();
 
-(async () => {
+(async() => {
     try {
         await sequelize.authenticate();
         client.login(process.env.DISCORD_TOKEN)
         await sequelize.sync();
 
-        setInterval(spoopy, 1000 * 60 * 20); // every 20 minutes
+        setInterval(spoopy, 1000 * 60 * 6); // every 20 minutes
     } catch (e) {
         console.log(e);
         logger.error(e);
