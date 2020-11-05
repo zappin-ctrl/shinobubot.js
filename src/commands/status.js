eb60eacf6f49c4089fde2ede0ded071209ae76a9
Ly <kakolp95@gmail.com>
2020-11-05 18:27:39 +0100

    Fix status uptime

--- src/commands/status.js
+++ src/commands/status.js
@@ -8,6 +8,6 @@ export const run = async (message) => {
 
     await message.channel.send(
         getEmbed()
-            .setDescription(`**Uptime:** ${moment().startOf('day').seconds(client.uptime / 1000).format('H:mm:ss')}\n\n**Servers:** \`${client.guilds.cache.size}\` - **Users:** \`${userCount}\``)
+            .setDescription(`**Uptime:** ${moment(client.uptime).format('HHH:mm:ss')}\n\n**Servers:** \`${client.guilds.cache.size}\` - **Users:** \`${userCount}\``)
     );
-};
+}
