98fa2b6d35fdbf4b03d36110338491f2f69317e7
Ly <kakolp95@gmail.com>
2020-11-10 21:08:45 +0100

    Update dependencies, fix status reporting incorrect time

--- src/commands/status.js
+++ src/commands/status.js
@@ -1,13 +1,20 @@
 import {getEmbed} from "../utility";
 import client from "../bot";
-import moment from "moment";
+
+const fill = (o) => o.toString().length >= 2 ? o.toString() : '0'+o.toString();
 
 export const run = async (message) => {
     let userCount = 0;
     client.guilds.cache.map(guild => userCount += guild.memberCount);
 
+    let total = Math.round(client.uptime / 1000);
+    const hours = Math.floor(total / 3600);
+    total %= 3600;
+    const minutes = Math.floor(total / 60);
+    const seconds = Math.floor(total % 60);
+
     await message.channel.send(
         getEmbed()
-            .setDescription(`**Uptime:** ${moment(client.uptime).format('HHH:mm:ss')}\n\n**Servers:** \`${client.guilds.cache.size}\` - **Users:** \`${userCount}\``)
+            .setDescription(`**Uptime:** ${fill(hours)+':'+fill(minutes)+':'+fill(seconds)}\n\n**Servers:** \`${client.guilds.cache.size}\` - **Users:** \`${userCount}\``)
     );
 }
