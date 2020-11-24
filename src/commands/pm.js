ea33c84ed1167dfb8e6cd8923ce5098b15491dae
zappin <61209204+zappin-ctrl@users.noreply.github.com>
2020-11-24 23:06:01 +0200

    Update pm.js

--- src/commands/pm.js
+++ src/commands/pm.js
@@ -7,14 +7,19 @@ export const run = async(message, args) => {
         return;
     }
     if (!args[0]) {
-        await message.reply("You need to provide a user's info.");
+        await message.reply("You need to provide a user's ID or ping them.");
         return;
     }
 
     const person = args[0].replace("<@", "").replace(">", "");
 
-    await client.users.cache.get(person).send(
-        `**\`From zappin#1312:\` ${args.join(" ").replace(args[0], "")}**`
-    );
+    try {
+        await client.users.cache.get(person).send(
+            `**\`From zappin#1312:\` ${args.join(" ").replace(args[0], "")}**`
+        );
+    } catch {
+    message.reply("That user doesn't exist.")
+    return;
+    }
     await message.channel.send(`Your message was sent ${process.env.EMOTE_VERIFIED}`);
 };
