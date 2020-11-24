e7d8e9a8989f9d08d1f82c5e4c9761697dc21d9c
zappin-ctrl <titos98@windowslive.com>
2020-11-24 23:01:13 +0200

    Stuff for xmas event & a thingie to reply to suggestions

---
+++ src/commands/pm.js
@@ -0,0 +1,20 @@
+import client from "../bot";
+import { isOwner } from "../utility";
+
+export const aliases = ["reply"];
+export const run = async(message, args) => {
+    if (!isOwner(message)) {
+        return;
+    }
+    if (!args[0]) {
+        await message.reply("You need to provide a user's info.");
+        return;
+    }
+
+    const person = args[0].replace("<@", "").replace(">", "");
+
+    await client.users.cache.get(person).send(
+        `**\`From zappin#1312:\` ${args.join(" ").replace(args[0], "")}**`
+    );
+    await message.channel.send(`Your message was sent ${process.env.EMOTE_VERIFIED}`);
+};
