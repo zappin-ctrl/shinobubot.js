905202e9062adbad574af8c442013e547ee0d01e
zappin-ctrl <titos98@windowslive.com>
2020-12-11 23:49:23 +0200

    Add joinedat command

---
+++ src/commands/joinedat.js
@@ -0,0 +1,60 @@
+import { getUserFromMention } from "../utility";
+
+function convertMS(milliseconds) {
+    var days, hour, minute, seconds, years, months, weeks;
+    seconds = Math.floor(milliseconds / 1000);
+    minute = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    hour = Math.floor(minute / 60);
+    minute = minute % 60;
+    days = Math.floor(hour / 24);
+    hour = hour % 24;
+    weeks = Math.floor(days / 7);
+    months = Math.floor(days / 30);
+    years = Math.floor(days / 365);
+    if (years) {
+        return `${years} year(s)`;
+    } else if (!years && months) {
+        return `${months} month(s)`;
+    } else if (!months && weeks) {
+        return `${weeks} week(s)`;
+    } else if (!weeks && days) {
+        return `${days} day(s)`;
+    } else if (!days && hour) {
+        return `${hour} hour(s)`;
+    } else if (!hour && minute) {
+        return `${minute} minute(s)`;
+    } else if (!minute && seconds) {
+        return `${seconds} second(s)`;
+    }
+}
+
+export const aliases = ["join", "joined", "joindate", "whendidigethere"];
+export const run = async(message, args) => {
+    let start = Date.now();
+    let end;
+    let complete;
+    if (!args[0]) {
+        end = message.member.joinedAt;
+        if (!end) {
+            await message.channel.send(`You can only use this command in a server.`)
+            return;
+        }
+        complete = (start - end);
+        await message.channel.send(`> 🛎 **You** joined **${convertMS(complete)}** ago!\n*(${end.toUTCString().replace(" GMT","")})*`);
+    } else {
+        let user = await getUserFromMention(args[0], message, true);
+        let memuser = message.channel.guild.member(user);
+        if (!user || !memuser) {
+            await message.channel.send(`That's not a valid member.`);
+            return;
+        }
+        end = memuser.joinedAt;
+        complete = start - end;
+        await message.channel.send(`> 🛎 **${memuser.user.username}** joined **${convertMS(complete)}** ago!\n*(${end.toUTCString().replace(" GMT","")})*`);
+    }
+};
+
+export const help = "Show when you/someone joined!";
+export const helpArguments = '[mention] (optional)';
+export const helpGroup = 'utility';
