affa423a12f955cfa8a09a2b0c6a77461741208b
pkly <kakolp95@gmail.com>
2021-01-24 19:01:50 +0100

    push basic changes for csharp

---
+++ bot/src/shinobu/Bot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace shinobu
+{
+    class Bot
+    {
+        private DiscordSocketClient client;
+
+        static void Main(string[] args)
+            => new Bot().Run(args).GetAwaiter().GetResult();
+
+        public async Task Run(string[] args)
+        {
+            this.client = new DiscordSocketClient();
+
+            await this.client.LoginAsync(Discord.TokenType.Bot, "");
+            await this.client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
